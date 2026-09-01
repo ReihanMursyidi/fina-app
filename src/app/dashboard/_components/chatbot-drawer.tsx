00c7fccb6ec49958b5374b4d1560eec5e767ab48
Reihan Mursyidi <mursyidireihan@gmail.com>
2026-09-01 17:24:02 +0700

    feat: enhance chatbot drawer with improved UI and add chatbot text area component

--- src/app/dashboard/_components/chatbot-drawer.tsx
+++ src/app/dashboard/_components/chatbot-drawer.tsx
@@ -1,23 +1,117 @@
 'use client';
 
+import { useState } from "react";
+import { BotIcon, XIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
-import { handleChat } from "@/features/ai/chat";
-import { BotIcon } from "lucide-react";
+import {
+   Drawer,
+   DrawerClose,
+   DrawerTrigger,
+   DrawerContent,
+   DrawerHeader,
+   DrawerTitle,
+   DrawerDescription,
+   DrawerFooter
+} from "@/components/ui/drawer";
+import { cn } from "@/lib/utils";
+import ChatbotTextArea from "./chatbot-textarea";
 
 export default function ChatbotDrawer() {
+   const [conversation, setConversation] = useState<
+      {
+         role: string;
+         parts: {
+            text: string;
+         }[];
+      }[]
+   >([
+      {
+         role: "user",
+         parts: [
+            {
+               text: 'Hello'
+            },
+         ],
+      },
+      {
+         role: 'model',
+         parts: [
+            {
+               text: 'Hi there! How can I assist you today?'
+            }
+         ]
+      }
+   ]);
+
    return (
-      <div className="fixed bottom-4 right-4">
-         <Button
-            className="flex items-center justify-center text-white rounded-full size-14 bg-primary"
-            size="icon-lg"
-            variant="outline"
-            onClick={async () => {
-               const result = await handleChat();
-               console.log(result);
-            }}   
-         >
-            <BotIcon className="size-6"  />
-         </Button>
-      </div>
+      <Drawer direction="right" modal={false}>
+         <DrawerTrigger className="fixed bottom-4 right-4" asChild>
+            <Button
+               className="rounded-full size-14 hover:bg-primary/30 hover:text-primary"
+               size="icon-lg"
+               variant="outline"
+            >
+               <BotIcon className="size-6" />
+            </Button>
+         </DrawerTrigger>
+
+         <DrawerContent className="w-screen! md:w-110!">
+            <DrawerHeader className="flex flex-row justify-between">
+               <div>
+                  <DrawerTitle className="font-bold text-primary">
+                     AI Financial Advisor
+                  </DrawerTitle>
+                  <DrawerDescription>
+                     Get personalized financial advice.
+                  </DrawerDescription>
+               </div>
+               <DrawerClose asChild>
+                  <Button variant="outline" size="icon">
+                     <XIcon />
+                  </Button>
+               </DrawerClose>
+            </DrawerHeader>
+
+            <div className="h-full px-4 overflow-y-auto no-scrollbar">
+               {conversation.length > 0 ? (
+                  <div className="flex flex-col h-full gap-8overflow-x-hidden overflow-y-auto no-scrollbar">
+                     {conversation.map((message, index) => (
+                        <div
+                           key={`conversation-${index}`}
+                           className={cn(
+                              'flex flex-col gap-2',
+                              message.role === 'model' ? 'items-start' : 'items-end',
+                           )}
+                        >
+                           <div
+                              className={cn('flex flex-col w-full', {
+                                 'bg-primary/20 text-primary px-5 py-2 rounded-3xl rounded-br-md w-fit max-w-3/4':
+                                    message.role === 'user',
+                              })}
+                           >
+                              {message.role === 'model' && (
+                                 <div className="flex items-center gap-1 text-xs font-semibold text-primary">
+                                    <BotIcon />
+                                    AI Advisor
+                                 </div>
+                              )}
+                              {message.parts[0].text}
+                           </div>
+                        </div>
+                     ))}
+                  </div>
+               ) : (
+                  <div className="flex flex-col items-center justify-center h-full">
+                     <h2 className="text-3xl font-bold text-primary">Welcome to your AI Financial Advisor!</h2>
+                     <h4 className="text-xl">Ask me anything about your finances!</h4>
+                  </div>
+               )}
+            </div>
+
+            <DrawerFooter>
+               <ChatbotTextArea />
+            </DrawerFooter>
+         </DrawerContent>
+      </Drawer>
    );
 }
