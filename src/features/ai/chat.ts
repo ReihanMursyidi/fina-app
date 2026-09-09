ebeeada689d65c7fafe1c61d2f1f436399bc92fa
Reihan Mursyidi <mursyidireihan@gmail.com>
2026-09-09 19:40:34 +0700

    feat: refactor chat handling to support transaction extraction and enhance response generation

--- src/features/ai/chat.ts
+++ src/features/ai/chat.ts
@@ -2,10 +2,9 @@
 
 import { createAI } from '@/features/ai/instance';
 import { Conversation } from '@/app/types/ai';
-import z from 'zod';
-import { createClient } from '@/lib/supabase/server';
-import { findEmbedding } from './embedding';
-import { Transaction } from '@/app/types/transaction';
+import { findEmbedding, generateEmbedding } from './embedding';
+import { getTransactionDeclaration } from './functionTransaction';
+import { Content, FunctionCall, Part } from '@google/genai';
 
 const SYSTEM_INSTRUCTION = `
    [Role]
@@ -97,16 +96,22 @@ export async function handleChat(
    return result;
 }
 
-async function generalChat(conversation: Conversation[], isThinking?: boolean) {
+async function generalChat(conversation: Content[], isThinking?: boolean) {
    const ai = createAI();
-   const response = await ai.models.generateContent({
-      model: 'gemini-3.5-flash',
+   const response = await ai.models.generateContentStream({
+      model: 'gemini-2.5-flash',
       contents: [...conversation],
       config: {
-         systemInstruction: SYSTEM_INSTRUCTION,
          thinkingConfig: {
             includeThoughts: isThinking,
          },
+         tools: [
+            {
+               googleSearch: {},
+               urlContext: {},
+            },
+         ],
+         systemInstruction: SYSTEM_INSTRUCTION,
          temperature: 0.2,
          topK: 5,
          topP: 0.1,
@@ -117,86 +122,14 @@ async function generalChat(conversation: Conversation[], isThinking?: boolean) {
    return response;
 }
 
-async function personalizedChat(
-   query: string, 
-   historyChat?: Conversation[], 
-   isThinking?: boolean,
-) {
-   const ai = createAI();
-
-   const data = await findEmbedding(query);
-
-   let contextData = '';
-
-   if(!data || data.length === 0) {
-      contextData = 'No transactions found that are similar or relevant';
-   } else {
-      contextData = data.map((transaction: Transaction) => {
-         return JSON.stringify(transaction);
-      })
-      .join('\n');
-   }
-
-   const prompt = `
-      <role>
-         You are an AI Financial Analyst. You are helping the user analyze 
-         their financial data using the RAG (Retrieval-Augmented Generation) technique.
-      </role>
-      <input>
-         User Question: "${query}"
-      </input>
-      <context>
-         Relevant Transaction data from the database (Ordered from most relevant):
-         ${contextData}
-      </context>
-      <instruction>
-         - Answer the user question ONLY based on the relevant transaction data above.
-         - If there are calculations (total spending, average, etc), calculate them accurately based on the data.
-         - Provide the answer in a neat, professional, yet easy-to-understand markdown format.
-         - If there is no relevant data at all, state that the data is not availble in the history.
-         - If user question is general and not need a data, response generally.
-      </instruction>
-      <constraints>
-         - Don't answer in table format instead of markdown.
-      </contraints>
-   `;
-
-   const response = await ai.models.generateContentStream({
-      model: 'gemini-3.5-flash',
-      contents: [
-         ...(historyChat ?? []),
-         { role: 'user', parts: [{ text: prompt }] },
-      ],
-      config: {
-         thinkingConfig: {
-            includeThoughts: isThinking,
-         },
-      },
-   });
-
-   return response;
-}
-
 export async function* handleChatStreaming(
-   conversation: Conversation[],
+   conversation: Content[],
    isThinking: boolean,
    mode: 'general' | 'personal',
 ) {
-   let response;
-
    if (mode === 'general') {
-      response = await generalChat(conversation, isThinking);
-   } else {
-      response = await personalizedChat(
-         conversation[conversation.length - 1].parts[0].text,
-         conversation.slice(0, -1),
-         isThinking,
-      );
-   }
+      const response = await generalChat(conversation, isThinking);
 
-   // Type Guard: Beritahu TypeScript untuk mengeksekusi blok ini 
-   // HANYA JIKA response terbukti sebagai stream (AsyncGenerator)
-   if (Symbol.asyncIterator in response) {
       if (isThinking) {
          for await (const chunk of response) {
             const parts = chunk.candidates?.[0]?.content?.parts;
@@ -220,81 +153,124 @@ export async function* handleChatStreaming(
          }
       }
    } else {
-      // Fallback jika API tiba-tiba mengembalikan respons tunggal (bukan stream)
-      const parts = response.candidates?.[0]?.content?.parts;
-      if (parts) {
-         for (const part of parts) {
-            if (part.thought && isThinking) {
-               yield `[thought]${part.text}`;
-            } else if (part.text) {
-               yield part.text;
+      const query = conversation[conversation.length - 1]?.parts?.[0].text;
+      const historyChat = conversation.slice(0, -1);
+      const ai = createAI();
+
+      const contents: Content[] = [
+         ...historyChat,
+         {
+            role: 'user',
+            parts: [
+               {
+                  text: `
+                  <role>
+                     You are an AI Financial Analyst. You are helping the user analyze their financial data.
+                  </role>
+                  <input>
+                     User Question: "${query}"
+                  </input>
+                  <instruction>
+                     - Extract the transaction details from the input.
+                     - Answer the user question ONLY based on the relevant transaction data (if there's need data).
+                     - If there are calculations (total spending, average, etc), calculate them accurately based on the data.
+                     - Provide the answer in a neat, professional, yet easy-to-understand markdown format.
+                     - If there is no relevant data at all, state that the data is not availble in the history.
+                     - If user question is general and not need a data, response generally.
+                     - The final response if there are no more functions being called is as simple as possible.
+                  </instruction>
+                  <context>
+                     Current Date : ${new Date().toISOString()}
+                  </context>
+                  <constraints>
+                     - Answer in relaxed, polite but professional in Indonesian.
+                     - Don't make assumptions about data from users if they don't mention it.
+                     - If there are questions outside the context related to finance, you must only answer questions related to finance.
+                     - Don't answer in table format instead of markdown.
+                  </contraints>
+                  `,
+               },
+            ],
+         },
+      ];
+
+      let running = true;
+      let iterate = 1;
+      while (running) {
+         iterate++;
+         const response = await ai.models.generateContentStream({
+            model: 'gemini-3.5-flash',
+            contents,
+            config: {
+               tools: [{ functionDeclarations: [getTransactionDeclaration] }],
+               thinkingConfig: {
+                  includeThoughts: isThinking,
+               },
+            },
+         });
+
+         const modelParts: Part[] = [];
+         const functionCalls: FunctionCall[] = [];
+
+         for await (const chunk of response) {
+            const parts = chunk.candidates?.[0]?.content?.parts || [];
+
+            if (parts) {
+               for (const part of parts) {
+                  modelParts.push(part);
+                  if (part.functionCall) {
+                     functionCalls.push(part.functionCall);
+                  } else if (part.text) {
+                     if (part.thought) {
+                        if (isThinking) yield `[thought]${part.text}`;
+                     } else {
+                        yield part.text;
+                     }
+                  } 
+               }
             }
+         }
+
+         if (functionCalls.length > 0) {
+            contents.push({ role: 'model', parts: modelParts });
+            const functionResponseParts = await Promise.all(
+               functionCalls.map(async (functionCall) => {
+                  const { name, args, id } = functionCall;
+                  if (!args) {
+                     throw new Error('No arguments provided for action');
+                  }
+   
+                  let resultData = {};
+   
+                  switch (name) {
+                     case 'get_transaction':
+                        const dataFind = await findEmbedding(
+                           JSON.stringify(args),
+                           0.3,
+                           100
+                        );
+                        resultData = dataFind || [];
+                        break;
+                     default:
+                        throw new Error(`Unknown function call`);
+                  }
+   
+                  return {
+                     functionResponse: {
+                        name,
+                        response: { result: resultData },
+                        id,
+                     },
+                  };
+               }),
+            );
+            contents.push({
+               role: 'user',
+               parts: functionResponseParts,
+            });
+         } else {
+            running = false;
          }
       }
    }
 }
-
-const transactionSchema = z.object({
-   amount: z.number().default(0).describe('Transaction nominal'),
-   type: z.enum(['income', 'expense']).describe('Type of transaction'),
-   category: z
-      .enum([
-         'Food & Drink',
-         'Shopping',
-         'Housing',
-         'Transportation',
-         'Entertainment',
-         'Salary',
-         'Others',
-      ])
-      .describe('Category of transaction'),
-   description: z.string().describe('Short text for describing transaction'),
-   date: z.string().describe('the date of transaction in YYYY-MM-DD format'),
-});
-
-export async function handleWizardInput(message: string) {
-   const contents = `
-   <role>
-      You are an AI Wizard finance assitant, who can extract transaction details from text.
-   </role>
-   <instruction>
-      Extract the transaction details from the following text and return it as a structure JSON object.
-      The JSON object must have exactly these fields:
-      - "amount": a number representing the cost (positive). Use 0 if not provided.
-      - "type": type of transaction, either 'income' or 'expense'.
-      - "category": choose the most appropriate category from this exact list:
-                     'Food & Drink','Shopping','Housing','Transportation','Entertainment','Salary','Others'.
-      - "description": a short string describing the transaction, first letter capitalized.
-      - "date": date of transaction in YYYY-MM-DD format.
-               Assume the current date if relative terms like 'today' or 'just now'. If not define use current date.
-   </instruction>
-   <context>
-      Current Date : ${new Date().toISOString()}
-   </context>
-   <input>
-      Text to extract: ${message}
-   </input>
-   <outputFormat>
-      Respond with only the raw JSON object, no markdown blocks, no text before or after.
-   </outputFormat>
-   `;
-
-   const ai = createAI();
-   const response = await ai.models.generateContent({
-      model: 'gemini-3.5-flash',
-      contents,
-      config: {
-         responseMimeType: 'application/json',
-         responseSchema: z.toJSONSchema(transactionSchema),
-      },
-   });
-
-   const transaction = transactionSchema.parse(JSON.parse(`${response.text}`));
-
-   if (transaction.amount <= 0) {
-      throw new Error('Cannot create transaction with invalid amount');
-   }
-
-   return transaction;
-}
-
