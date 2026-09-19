21e918c05347ef8f955ed092d8fc226716c12746
Reihan Mursyidi <mursyidireihan@gmail.com>
2026-09-19 19:11:33 +0700

    fix: ensure allowedDevOrigins is correctly defined in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: ['10.26.1.111'],
   /* config options here */
   reactCompiler: true,
   experimental: {
